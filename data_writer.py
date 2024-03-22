174304897253b7b9f467edf46fa3aefac13f7b3c
marcopibbes <marco.bardazzi1@stud.unifi.it>
2024-03-22 11:51:50 +0100

    Added .csv export of bbox trackings

--- data_writer.py
+++ data_writer.py
@@ -1,4 +1,14 @@
 import csv
+import open3d as o3d
 
-def write_bbox_csv(bounding_boxes):
-    a
+def write_bbox_csv(timestamp,id,x1,y1,z1,x2,y2,z2,x3,y3,z3,x4,y4,z4,x5,y5,z5,x6,y6,z6,x7,y7,z7,x8,y8,z8):
+
+    #new_data=['timestamp','id','x1','y1','z1','x2','y2','z2','x3','y3','z3','x4','y4','z4','x5','y5','z5','x6','y6','z6','x7','y7','z7','x8','y8','z8']
+    new_data=[timestamp,id,x1,y1,z1,x2,y2,z2,x3,y3,z3,x4,y4,z4,x5,y5,z5,x6,y6,z6,x7,y7,z7,x8,y8,z8]
+    
+    with open("data.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(new_data)
+
+    file.close()
+
--- tracking.py
+++ tracking.py
@@ -2,6 +2,8 @@ import numpy as np
 import open3d as o3d
 import random
 import matplotlib.pyplot as plt
+import csv
+import data_writer
 
 
 def plot_trajectories(bounding_boxes):
@@ -18,27 +20,36 @@ def plot_trajectories(bounding_boxes):
     plt.xlabel("X-axis")
     plt.ylabel("Y-axis")
     plt.title("Detected Trajectories")
-
-    
     plt.show()
 
             
         
 def track(bounding_boxes):
 
-    
+
     for list in bounding_boxes:
         for bbox in list:
             bbox.color=[round(random.random(),1),round(random.random(),1),round(random.random(),1)]
         
 
     matched_bounding_boxes=[]
+    map={}
+    
 
     for i in range(len(bounding_boxes)-1) : 
 
+        
+        
         for j in range (len(bounding_boxes[i])):
-
+            
+            
+            
             corners1 = bounding_boxes[i][j].get_box_points()
+
+            if(i==0):
+             map[bounding_boxes[i][j].color[0]+bounding_boxes[i][j].color[2]+bounding_boxes[i][j].color[1]]=random.randint(0,5000)
+             data_writer.write_bbox_csv(i+20,map[bounding_boxes[i][j].color[0]+bounding_boxes[i][j].color[2]+bounding_boxes[i][j].color[1]],np.asarray(corners1)[0,0],np.asarray(corners1)[0,1],np.asarray(corners1)[0,2],np.asarray(corners1)[1,0],np.asarray(corners1)[1,1],np.asarray(corners1)[1,2],np.asarray(corners1)[2,0],np.asarray(corners1)[2,1],np.asarray(corners1)[2,2],np.asarray(corners1)[3,0],np.asarray(corners1)[3,1],np.asarray(corners1)[3,2],np.asarray(corners1)[4,0],np.asarray(corners1)[4,1],np.asarray(corners1)[4,2],np.asarray(corners1)[5,0],np.asarray(corners1)[5,1],np.asarray(corners1)[5,2],np.asarray(corners1)[6,0],np.asarray(corners1)[6,1],np.asarray(corners1)[6,2],np.asarray(corners1)[7,0],np.asarray(corners1)[7,1],np.asarray(corners1)[7,2])
+
         
             for bbox in bounding_boxes[i+1]:
                 
@@ -61,10 +72,12 @@ def track(bounding_boxes):
 
                     if (xmin_overlap >= xmax_overlap) or (ymin_overlap >= ymax_overlap) and (bbox!=bounding_boxes[i]):
                         bbox.color=[round(random.random(),1),round(random.random(),1),round(random.random(),1)]
+                        
 
                     else:
                         bbox.color=bounding_boxes[i][j].color
                         matched_bounding_boxes.append(bbox)
+                        data_writer.write_bbox_csv(i+21,map[bbox.color[0]+bbox.color[2]+bbox.color[1]],np.asarray(corners2)[0,0],np.asarray(corners2)[0,1],np.asarray(corners2)[0,2],np.asarray(corners2)[1,0],np.asarray(corners2)[1,1],np.asarray(corners2)[1,2],np.asarray(corners2)[2,0],np.asarray(corners2)[2,1],np.asarray(corners2)[2,2],np.asarray(corners2)[3,0],np.asarray(corners2)[3,1],np.asarray(corners2)[3,2],np.asarray(corners2)[4,0],np.asarray(corners2)[4,1],np.asarray(corners2)[4,2],np.asarray(corners2)[5,0],np.asarray(corners2)[5,1],np.asarray(corners2)[5,2],np.asarray(corners2)[6,0],np.asarray(corners2)[6,1],np.asarray(corners2)[6,2],np.asarray(corners2)[7,0],np.asarray(corners2)[7,1],np.asarray(corners2)[7,2])
 
     total_bounding_boxes=[]
     for row in bounding_boxes:
